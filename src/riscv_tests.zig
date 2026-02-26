d5306e07ac649b4ee05f5dc6f01ea44f1005b26c
Dhole <dhole@riseup.net>
2026-02-26 20:12:08 +0100

    fix tests

--- src/riscv_tests.zig
+++ src/riscv_tests.zig
@@ -30,19 +30,46 @@ test "cpu" {
     _ = cpu.exec(inst);
 }
 
-fn test_load_bin(cpu: *Cpu, bin_path: []const u8) !void {
-    var rom_file = try std.fs.cwd().openFile(bin_path, .{ .mode = .read_only });
-    defer rom_file.close();
+fn test_load_elf(allocator: Allocator, cpu: *Cpu, elf_path: []const u8) !void {
+    var elf = try elfy.Elf.init(elf_path, .ReadOnly, allocator);
+    defer elf.deinit();
 
-    const rom_file_size = (try rom_file.stat()).size;
-    var reader = rom_file.reader(cpu.mem.rom);
-    try reader.interface.fill(rom_file_size);
+    cpu.pc = @intCast(elf.getHeader().getEntryPoint());
+
+    const rom_addr = cpu.mem.rom_offset;
+    const rom_size = cpu.mem.rom.len;
+    const ram_addr = cpu.mem.ram_offset;
+    const ram_size = cpu.mem.ram.len;
+
+    var segments = try elf.getIterator(elfy.ElfProgram);
+    while (try segments.next()) |segment| {
+        if (segment.getType() != .PT_LOAD) {
+            continue;
+        }
+        const vaddr = segment.getVirtualAddress();
+        const vsize = segment.getMemorySize();
+        const size = segment.getFileSize();
+        try expectEqual(size, vsize);
+        const flags = segment.getFlags();
+        if (!flags.write() & ((rom_addr <= vaddr) & (vaddr + vsize <= rom_addr + rom_size))) {
+            const data = try elf.getProgramData(segment);
+            const offset = vaddr - rom_addr;
+            @memcpy(cpu.mem.rom[offset .. offset + data.len], data);
+        } else if (flags.write() & ((ram_addr <= vaddr) & (vaddr + vsize <= ram_addr + ram_size))) {
+            const data = try elf.getProgramData(segment);
+            const offset = vaddr - ram_addr;
+            @memcpy(cpu.mem.ram[offset .. offset + data.len], data);
+        } else {
+            std.debug.panic("Unexpected segment config", .{});
+        }
+    }
 }
 
-fn test_exec_bin(allocator: Allocator, bin_path: []const u8) !Cpu {
+fn test_exec_elf(allocator: Allocator, elf_path: []const u8) !Cpu {
     var cpu = try test_cpu_init(allocator);
-    try test_load_bin(&cpu, bin_path);
-    cpu.pc = 0x80000000;
+    try test_load_elf(allocator, &cpu, elf_path);
+    // std.debug.print("RAM:\n{f}", .{MemoryFmt{ .mem = &cpu.mem, .offset = 0x80001000, .size = 0x100 }});
+
     var i: usize = 0;
     while (i < 0x1000) : (i += 1) {
         const word = cpu.mem.read(u32, cpu.pc);
@@ -77,7 +104,7 @@ test "rv32ui" {
         "blt",
         "bltu",
         "bne",
-        // "fence_i", // FIXME: Requires different ELF sections
+        "fence_i",
         "jal",
         "jalr",
         "lb",
@@ -87,11 +114,11 @@ test "rv32ui" {
         "lhu",
         "lui",
         "lw",
-        // "ma_data", // FIXME
+        "ma_data", // FIXME
         "or",
         "ori",
-        // "sb", // FIXME
-        // "sh", // FIXME
+        "sb", // FIXME
+        "sh", // FIXME
         "simple",
         "sll",
         "slli",
@@ -103,17 +130,17 @@ test "rv32ui" {
         "srai",
         "srl",
         "srli",
-        // "st_ld", // FIXME
+        "st_ld", // FIXME
         "sub",
-        // "sw", // FIXME
+        "sw", // FIXME
         "xor",
         "xori",
     };
     for (ops_str) |op_str| {
         std.debug.print("> {s}\n", .{op_str});
-        const bin_path = try std.fmt.allocPrint(allocator, "riscv-tests/isa/rv32ui-p-{s}.bin", .{op_str});
+        const elf_path = try std.fmt.allocPrint(allocator, "riscv-tests/isa/rv32ui-p-{s}", .{op_str});
 
-        const cpu = try test_exec_bin(allocator, bin_path);
+        const cpu = try test_exec_elf(allocator, elf_path);
         try expectEqual(0, cpu.regs[10]);
     }
 }
@@ -134,79 +161,21 @@ test "rv32um" {
     };
     for (ops_str) |op_str| {
         std.debug.print("> {s}\n", .{op_str});
-        const bin_path = try std.fmt.allocPrint(allocator, "riscv-tests/isa/rv32um-p-{s}.bin", .{op_str});
+        const elf_path = try std.fmt.allocPrint(allocator, "riscv-tests/isa/rv32um-p-{s}", .{op_str});
 
-        const cpu = try test_exec_bin(allocator, bin_path);
+        const cpu = try test_exec_elf(allocator, elf_path);
         try expectEqual(0, cpu.regs[10]);
     }
-}
-
-fn test_exec_elf(allocator: Allocator, elf_path: []const u8) !Cpu {
-    var cpu = try test_cpu_init(allocator);
-    try test_load_elf(allocator, &cpu, elf_path);
-    // std.debug.print("RAM:\n{f}", .{MemoryFmt{ .mem = &cpu.mem, .offset = 0x80001000, .size = 0x100 }});
-
-    var i: usize = 0;
-    while (i < 0x1000) : (i += 1) {
-        const word = cpu.mem.read(u32, cpu.pc);
-        const inst = Inst.init(word);
-        // std.debug.print("{x:0>8}:  {x:0>8}  ", .{ cpu.pc, word });
-        // std.debug.print("{f}\n", .{InstFmt{ .addr = cpu.pc, .inst = inst }});
-        const opt_trap = cpu.exec(inst);
-        if (opt_trap) |trap| {
-            if (trap == .ecall) {
-                return cpu;
-            } else {
-                return error.unexpected_trap;
-            }
-        }
-    }
-    return error.too_many_steps;
 }
 
 test "rv32_single" {
     var gpa: std.heap.DebugAllocator(.{}) = .init;
     const allocator = gpa.allocator();
 
-    const elf_path: []const u8 = "riscv-tests/isa/rv32ui-p-st_ld";
+    const elf_path: []const u8 = "riscv-tests/isa/rv32ui-p-ld_st";
 
     const cpu = try test_exec_elf(allocator, elf_path);
     try expectEqual(0, cpu.regs[10]);
-}
-
-fn test_load_elf(allocator: Allocator, cpu: *Cpu, elf_path: []const u8) !void {
-    var elf = try elfy.Elf.init(elf_path, .ReadOnly, allocator);
-    defer elf.deinit();
-
-    cpu.pc = @intCast(elf.getHeader().getEntryPoint());
-
-    const rom_addr = cpu.mem.rom_offset;
-    const rom_size = cpu.mem.rom.len;
-    const ram_addr = cpu.mem.ram_offset;
-    const ram_size = cpu.mem.ram.len;
-
-    var segments = try elf.getIterator(elfy.ElfProgram);
-    while (try segments.next()) |segment| {
-        if (segment.getType() != .PT_LOAD) {
-            continue;
-        }
-        const vaddr = segment.getVirtualAddress();
-        const vsize = segment.getMemorySize();
-        const size = segment.getFileSize();
-        try expectEqual(size, vsize);
-        const flags = segment.getFlags();
-        if (!flags.write() & ((rom_addr <= vaddr) & (vaddr + vsize <= rom_addr + rom_size))) {
-            const data = try elf.getProgramData(segment);
-            const offset = vaddr - rom_addr;
-            @memcpy(cpu.mem.rom[offset .. offset + data.len], data);
-        } else if (flags.write() & ((ram_addr <= vaddr) & (vaddr + vsize <= ram_addr + ram_size))) {
-            const data = try elf.getProgramData(segment);
-            const offset = vaddr - ram_addr;
-            @memcpy(cpu.mem.ram[offset .. offset + data.len], data);
-        } else {
-            std.debug.panic("Unexpected segment config", .{});
-        }
-    }
 }
 
 test "elfy" {
